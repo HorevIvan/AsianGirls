77fc485bb123d2242d2d627c0d79022bf82991f1
KHorev Ivan <Ivan.Khorev@transwareag.com>
2015-08-24 13:56:35 +0700

    Рефакторинг

--- ZendeskPrototype/ZendeskPrototype/Program.cs
+++ ZendeskPrototype/ZendeskPrototype/Program.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ZendeskApi_v2;
+using ZendeskApi_v2.Models.Constants;
+using ZendeskApi_v2.Models.Tickets;
 
 namespace ZendeskPrototype
 {
     class Program
     {
         static void Main()
+        {
+            var api = GetApi();
+
+            DisplayViews(api);
+
+            Console.ReadLine();
+        }
+
+        private static ZendeskApi GetApi()
         {
             Console.WriteLine("Enter path");
 
@@ -22,17 +33,32 @@ namespace ZendeskPrototype
 
             var password = Console.ReadLine();
 
-            var api = new ZendeskApi(path + "/api/v2", email, password);
+            return CreateApi(path + "/api/v2", email, password);
+        }
 
-            foreach (var view in api.Views.GetAllViews().Views)
+        private static ZendeskApi CreateApi(String path, String email, String password)
+        {
+            Console.WriteLine("--Api-----");
+
+            return new ZendeskApi(path, email, password);
+        }
+
+        private static void DisplayViews(ZendeskApi api)
+        {
+            Console.WriteLine("--Views-----");
+
+            var views = api.Views.GetAllViews().Views;
+
+            foreach (var view in views)
             {
                 Console.Write(view.Id);
+
                 Console.Write("\t");
+
                 Console.Write(view.Title);
+
                 Console.WriteLine();
             }
-
-            Console.ReadLine();
         }
     }
 }
